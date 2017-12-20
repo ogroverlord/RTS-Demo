39985c30990e19d6f5235bdfaaeb3a80603797fa
ogroverlord <karol_michalczyk@interia.pl>
2017-12-20 22:31:19 +0100

    Own NetworkManager added

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking; 
+
+
+public class MyNetworkManager : NetworkManager {
+
+    public void MyStartHost()
+    {
+        StartHost();
+        Debug.Log("Starting Host at: " + Time.timeSinceLevelLoad); 
+    }
+
+    public override void OnStartHost()
+    {
+        base.OnStartHost();
+        Debug.Log("Host started at :" + Time.timeSinceLevelLoad);
+    }
+
+}
