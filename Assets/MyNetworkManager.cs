18387f7783b40963317412d256aed2cf4ba671cc
ogroverlord <karol_michalczyk@interia.pl>
2018-01-02 21:57:28 +0100

    End of the course state

--- Assets/MyNetworkManager.cs
+++ Assets/MyNetworkManager.cs
@@ -9,13 +9,31 @@ public class MyNetworkManager : NetworkManager {
     public void MyStartHost()
     {
         StartHost();
-        Debug.Log("Starting Host at: " + Time.timeSinceLevelLoad); 
+        Debug.Log(Time.timeSinceLevelLoad + "staring Host."); 
     }
 
     public override void OnStartHost()
     {
         base.OnStartHost();
-        Debug.Log("Host started at :" + Time.timeSinceLevelLoad);
+        Debug.Log( Time.timeSinceLevelLoad + " Host started");     
+    }
+
+
+    public override void OnStartClient(NetworkClient myClient)
+    {
+        Debug.Log( Time.timeSinceLevelLoad + " client start requested.");
+        InvokeRepeating("Loading", 0f, 0.01f);
+    }
+
+    public override void OnClientConnect(NetworkConnection connection)
+    {
+        CancelInvoke("Loading"); 
+        Debug.Log(Time.timeSinceLevelLoad + " Client is connected to IP: " + connection.address);
+    }
+
+    public void Loading()
+    {
+      Debug.Log("...");
     }
 
 }
---
+++ Assets/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : NetworkBehaviour {
+
+    private Vector3 inputValue;
+    private const float scale = 0.2f;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        inputValue.x = scale * CrossPlatformInputManager.GetAxis("Horizontal");
+        inputValue.y = 0f; 
+        inputValue.z = scale * CrossPlatformInputManager.GetAxis("Vertical");
+
+        transform.Translate(inputValue);
+
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponentInChildren<Camera>().enabled = true; 
+    }
+}
